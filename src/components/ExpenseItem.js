2ee8fcddbb1b38191ad4fe8121e1faf633bc362f
ashrafstakala <ashrafstakala@gmail.com>
2021-12-23 23:32:17 +0200

    added expense css

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
-  return <h2>Expense item!</h2>;
+  return (
+    <div className="expense-item">
+      <div>23 December 20201</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+      </div>
+      <div className="expense-item__price">$299.99</div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
